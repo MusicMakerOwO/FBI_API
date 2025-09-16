508c395f1a6872630b004a6519d3debeb1fd800a
Music Maker <joshuanejdl@gmail.com>
2025-09-16 16:36:12 -0500

    refactor: Updated FingerprintRequest() for future use

--- src/Utils/FingerprintRequest.ts
+++ src/Utils/FingerprintRequest.ts
@@ -1,10 +1,20 @@
 import { Request } from 'express';
+import crypto from 'crypto';
+import ResolveIP from "./ResolveIP";
 
+/**
+ * Generate a device fingerprint from a request. \
+ * This is not at all reliable because a lot of this information is generic and
+ * can be spoofed, but it will defend against basic attacks and script kiddies.
+ * A given request will always generate the same fingerprint, this is then hashed and the IP is included.
+ * @param req
+ * @returns {string} A SHA-1 hash of the fingerprint.
+ */
 export async function FingerprintRequest(req: Request) {
-	return {
-		user_agent: String(req.headers['user-agent'] || ''),
-		accept: String(req.headers['accept'] || ''),
-		accept_language: String(req.headers['accept-language'] || ''),
-		accept_encoding: String(req.headers['accept-encoding'] || '')
-	}
+	const IP = ResolveIP(req);
+	const userAgent = req.headers['user-agent'] || '';
+	const accept = req.headers['accept'] || '';
+	const acceptLanguage = req.headers['accept-language'] || '';
+	const acceptEncoding = req.headers['accept-encoding'] || '';
+	return crypto.createHash('sha1').update(IP + ':' + userAgent + ':' + accept + ':' + acceptLanguage + ':' + acceptEncoding).digest('hex');
 }
