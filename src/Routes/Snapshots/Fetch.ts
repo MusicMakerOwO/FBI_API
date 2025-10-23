e7d92282809d73b05f079317066b8bd497744274
Music Maker <joshuanejdl@gmail.com>
2025-10-23 14:32:44 -0500

    feat: Secured snapshot fetching with guild access (no permissions yet)

--- src/Routes/Snapshots/Fetch.ts
+++ src/Routes/Snapshots/Fetch.ts
@@ -1,6 +1,8 @@
 import {IEndpoint} from '../../Typings/Types';
 import {Request, Response} from 'express';
-import {FetchSnapshot} from "../../Utils/SnapshotUtils";
+import {FetchSnapshot, ResolveGuildIDFromSnapshot} from "../../Utils/SnapshotUtils";
+import {ResolveToken} from "../../Utils/OAuth/ResolveToken";
+import {GetGuilds} from "../../Utils/OAuth/UserRoutes";
 
 export default {
 	method: 'GET',
@@ -11,10 +13,16 @@ export default {
 		const snapshotID = parseInt('0' + req.query.id, 10);
 		if (snapshotID < 1) return { status: 400, message: 'Snapshot ID must be a positive integer' }
 
-		console.time('Snapshot generation')
-		const snapshot = await FetchSnapshot(snapshotID);
-		console.timeEnd('Snapshot generation')
+		const guildID = await ResolveGuildIDFromSnapshot(snapshotID);
+		if (!guildID) return { status: 404, message: 'Snapshot not found' };
 
-		return snapshot;
+		const [ token, expiresAt ] = ResolveToken(req);
+		if (expiresAt < Date.now()) return { status: 401, message: 'Session expired' };
+
+		const guilds = await GetGuilds(token);
+
+		if (!guilds.has(guildID)) return { status: 401, message: 'You are not a member of this server' }
+
+		return await FetchSnapshot(snapshotID);
 	}
 } as IEndpoint;
