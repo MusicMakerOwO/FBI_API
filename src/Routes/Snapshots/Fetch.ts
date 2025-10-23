361f30398fc5d33e29d1aaeaef1f272bdec8bb8a
Music Maker <joshuanejdl@gmail.com>
2025-10-23 16:19:11 -0500

    feat: Secured snapshot fetching with guild member permissions

--- src/Routes/Snapshots/Fetch.ts
+++ src/Routes/Snapshots/Fetch.ts
@@ -3,6 +3,9 @@ import {Request, Response} from 'express';
 import {FetchSnapshot, ResolveGuildIDFromSnapshot} from "../../Utils/SnapshotUtils";
 import {ResolveToken} from "../../Utils/OAuth/ResolveToken";
 import {GetGuilds} from "../../Utils/OAuth/UserRoutes";
+import {FetchDiscordGuild, FetchDiscordMember, FetchOAuthUser} from "../../Utils/Discord/FetchDiscord";
+import {GlobalMemberPermissions} from "../../Utils/Discord/MemberPermissions";
+import {DISCORD_PERMISSIONS} from "../../Utils/Discord/Permissions";
 
 export default {
 	method: 'GET',
@@ -19,9 +22,21 @@ export default {
 		const [ token, expiresAt ] = ResolveToken(req);
 		if (expiresAt < Date.now()) return { status: 401, message: 'Session expired' };
 
-		const guilds = await GetGuilds(token);
+		const userGuilds = await GetGuilds(token);
+		if (!userGuilds.has(guildID)) return { status: 401, message: 'You are not a member of this server' }
 
-		if (!guilds.has(guildID)) return { status: 401, message: 'You are not a member of this server' }
+		// so many API calls sob
+		const user = await FetchOAuthUser(token);
+		const guild = await FetchDiscordGuild(guildID);
+		const member = await FetchDiscordMember(guildID, user.id);
+
+		// can be very expensive to call in some cases
+		// thankfully guild fetching caches all roles automatically
+		const permissions = await GlobalMemberPermissions(guild, member);
+
+		if ((permissions & DISCORD_PERMISSIONS.MANAGE_GUILD) === 0n) {
+			return { status: 403, message: 'You do not have permission to access snapshots' };
+		}
 
 		return await FetchSnapshot(snapshotID);
 	}
