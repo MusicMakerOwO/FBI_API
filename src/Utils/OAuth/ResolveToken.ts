04e7bbdbc1b73089ad4baa5afa5cee756db83015
Music Maker <joshuanejdl@gmail.com>
2025-10-24 12:59:14 -0500

    fix: Decode header session token if provided

--- src/Utils/OAuth/ResolveToken.ts
+++ src/Utils/OAuth/ResolveToken.ts
@@ -6,8 +6,9 @@ const tokenCache = new WeakMap<Request, [string, number]>();
 export function ResolveToken(req: Request): [ string, number ] {
 	if (tokenCache.has(req)) return tokenCache.get(req)!;
 
-	const cookieJar = new Cookies(req.headers.cookie);
-	const encryptedToken = req.headers.session ?? cookieJar.get('session'); // Support both header and cookie for flexibility
+	const headerSession = typeof req.headers.session === 'string' ? decodeURIComponent(req.headers.session.replace(/\+/g, ' ')) : undefined;
+
+	const encryptedToken = headerSession ?? new Cookies(req.headers.cookies).get('session'); // Support both header and cookie for flexibility
 	if (!encryptedToken) return [ '', 0 ];
 
 	const tokenBuffer = Buffer.from(encryptedToken, 'base64');
