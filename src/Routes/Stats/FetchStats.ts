af80b722e88a72b8271bd06201df61b6827e7ef0
Music Maker <joshuanejdl@gmail.com>
2025-10-25 00:25:47 -0500

    refactor: Inline variable for return

--- src/Routes/Stats/FetchStats.ts
+++ src/Routes/Stats/FetchStats.ts
@@ -5,16 +5,13 @@ export default {
 	route: '/stats',
 	method: 'GET',
 	handler: async (req: Request, res: Response) => {
-		const stats = await Database.query(`
-			SELECT 
-				SUM(guilds) as guilds,
-				SUM(messages) as messages,
-				SUM(users) as users,
-				SUM(snapshots) as snapshots,
-				MAX(updated_at) as last_updated
-			FROM BotStats
+		return await Database.query(`
+            SELECT SUM(guilds)     as guilds,
+                   SUM(messages)   as messages,
+                   SUM(users)      as users,
+                   SUM(snapshots)  as snapshots,
+                   MAX(updated_at) as last_updated
+            FROM BotStats
 		`).then(x => x[0]);
-
-		return stats;
 	}
 }
